c85cb39004f4eb12ff822f1543033153786d49cf
Nacho369 <nachodev369@gmail.com>
2022-10-15 09:39:02 -0700

    Update v1

--- _printf.c
+++ _printf.c
@@ -2,13 +2,59 @@
 #include <stdarg.h>
 #include "main.h"
 
+int p_char(va_list args);
+
 /**
- * _printf - Produce oitput according to specifies
+ * _printf - Produce output according to specified
  * format
  *
  * Return: void
  */
-void _printf()
+int _printf(const char *format, ...)
 {
+	int i, j, len = 0;
 
+	va_list arg_param;
+
+	args_t types[] = {
+		{'c', p_char}
+	};
+
+	if (format == NULL || (format[0] == '%' && format[1] == 0))
+		return (-1);
+
+	va_start(arg_param, format);
+
+	i = 0;
+
+	while (format[i] && format != NULL)
+	{
+		j = 0;
+
+		while (j < 2)
+		{
+			if (format[i] == types[j].ch)
+			{
+				len++;
+				types[j].dt(arg_param);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	_putchar('\n');
+
+	va_end(arg_param);
+
+	return (len);
+}
+
+int p_char(va_list args)
+{
+	int ch = va_arg(args, int);
+
+	_putchar(ch);
+
+	return (1);
 }
--- main.h
+++ main.h
@@ -1,4 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+
+typedef struct arg_types
+{
+	char ch;
+	int (*dt)(va_list args);
+}args_t;
+
 #endif
