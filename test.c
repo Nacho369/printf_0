e1e499fe99e2f5037995bfdfedf4d3ba4669be43
Nacho369 <nachodev369@gmail.com>
2022-10-16 12:59:06 -0700

    Task 0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- test.c
+++ test.c
@@ -9,11 +9,18 @@
  */
 int main(void)
 {
+	int len;
 	char *str = "Hello There!";
 	
-	_printf("This prints a character :  [%c]\n", 'G');
+	_printf("\nThis prints a character :  [%c]\n", 'G');
 	_printf("This prints a string :  [%s]\n", str);
-	_printf("This is escape percent [%%]\n");
+	_printf("This is escape percent [%%]\n\n");
+
+	len = _printf("This is a test!");
+
+	_printf("\n\n");
+
+	printf("%d\n\n", len);
 	
 	return (0);
 }
