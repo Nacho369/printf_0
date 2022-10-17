246086c6cc4042971b3e05bc54131ae45e1a1da8
Nacho369 <nachodev369@gmail.com>
2022-10-17 02:00:48 -0700

    Task 0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- main.h
+++ main.h
@@ -1,14 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+
+/* Main Functions */
 int _putchar(char c);
 
 int _printf(const char *format, ...);
+
 
 typedef struct arg_types
 {
 	char ch;
 	int (*dt)(va_list args);
 }args_t;
+
+/* Format Printers */
+int p_char(va_list args);
+
+int p_str(va_list args);
 
 #endif
