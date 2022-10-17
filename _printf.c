9ff3a8b03ef5f9d8fe64dcde84d0eeebd2c73636
Nacho369 <nachodev369@gmail.com>
2022-10-17 01:09:36 -0700

    Task 0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,55 +1,20 @@
+#include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
+
+int p_char(va_list args);
+int p_str(va_list args);
+
 /**
-<<<<<<< HEAD
- * get_op - select function for conversion char
- * @c: char to check
- * Return: pointer to function
+ * _printf - Produce output according to specified
+ * format
+ *
+ * Return: void
  */
-
-<<<<<<< HEAD
-int (*get_op(const char c))(va_list)
-=======
-  * find_function - function that finds formats for _printf
-  * calls the corresponding function.
-  * @format: format (char, string, int, decimal)
-  * Return: NULL or function associated ;
-  */
-int (*find_function(const char *format))(va_list)
->>>>>>> Alafara
-{
-	unsigned int i = 0;
-	code_f find_f[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"i", print_int},
-		{"d", print_dec},
-		{"r", print_rev},
-		{"b", print_bin},
-		{"u", print_unsig},
-		{"o", print_octal},
-		{"x", print_x},
-		{"X", print_X},
-		{"R", print_rot13},
-		{NULL, NULL}
-	};
-
-	while (find_f[i].sc)
-	{
-		if (find_f[i].sc[0] == (*format))
-			return (find_f[i].f);
-		i++;
-	}
-	return (NULL);
-}
-/**
-  * _printf - function that produces output according to a format.
-  * @format: format (char, string, int, decimal)
-  * Return: size the output text;
-  */
 int _printf(const char *format, ...)
-=======
+{
+	int i, j, len = 0;
+
 	va_list arg_param;
 
 	args_t types[] = {
@@ -115,52 +80,19 @@ int p_char(va_list args)
  * Return: Lenght of character printed
  */
 int p_str(va_list args)
->>>>>>> 577c5428389078bd1473433d7ca0821f03e1a930
 {
-	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0, cprint = 0;
+	int i, j;
+	char n[] = "(null)";
+	char *s = va_arg(args, char *);
 
-	if (format == NULL)
-		return (-1);
-	va_start(ap, format);
-	while (format[i])
+	if (s == NULL)
 	{
-		while (format[i] != '%' && format[i])
-		{
-			_putchar(format[i]);
-			cprint++;
-			i++;
-		}
-		if (format[i] == '\0')
-			return (cprint);
-		f = find_function(&format[i + 1]);
-		if (f != NULL)
-		{
-			cprint += f(ap);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		cprint++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
+		for (i = 0; n[i] != '\0'; i++)
+			_putchar(n[i]);
+		return (6);
 	}
-<<<<<<< HEAD
-	va_end(ap);
-<<<<<<< HEAD
-	return (sum);
-=======
 	for (j = 0; s[j] != '\0'; j++)
 		_putchar(s[j]);
 
 	return (j);
->>>>>>> 
-=======
-	return (cprint);
->>>>>>> Alafara
 }
