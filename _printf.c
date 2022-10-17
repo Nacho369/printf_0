ef3bbeb35f3f246673b9414c3dd48ea73675d556
Nacho369 <nachodev369@gmail.com>
2022-10-17 03:11:56 -0700

    Task 0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -8,59 +8,42 @@
  */
 int _printf(const char *format, ...)
 {
-	int i, j, count, len = 0;
+	int i = 0, j, len = 0, count;
+	va_list valist;
+	types difftypes[] = {{'c', t_char}, {'s', t_string}, {'d', print_number},
+			     {'i', print_number}, {'b', binary}, {'u', print_unsigned},
+			     {'x', hexa}, {'X', hexa_upper}, {'o', octal}, {'R', print_rot},
+			     {'r', print_rev}, {'S', stringhexa}, {'p', pointer}};
 
-	va_list arg_param;
-
-	args_t types[] = {
-		{'c', p_char},
-		{'s', p_str}
-	};
-
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == 0))
 		return (-1);
-
-	va_start(arg_param, format);
-
-	i = 0;
-
+	va_start(valist, format);
 	while (format != NULL && format[i])
 	{
 		if (format[i] != '%')
-		{
 			len += _putchar(format[i]);
-		}
 		else
 		{
 			i++;
 			if (format[i] == '%')
-			{
 				len += _putchar('%');
-			}
-
+			j = 0;
 			count = 0;
-
-			for (j = 0; j < 2; j++)
+			while (j < 13)
 			{
-				if (format[i] == types[j].ch)
+				if (format[i] == difftypes[j].t)
 				{
-					len += types[j].dt(arg_param);
+					len += difftypes[j].f(valist);
 					count = 1;
-					break;
-				}
-			}
+					break; }
+				j++; }
 			if (!count && format[i] != '%')
 			{
 				len++;
 				len++;
 				_putchar('%');
-				_putchar(format[i]);
-			}
-		}
-		i++;
-	}
-
-	va_end(arg_param);
-
+				_putchar(format[i]); }}
+		i++; }
+	va_end(valist);
 	return (len);
 }
