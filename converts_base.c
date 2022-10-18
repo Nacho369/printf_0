874c09db366ca5d90c46f0825f290919a605b36a
Nacho369 <nachodev369@gmail.com>
2022-10-17 23:54:46 -0700

    Task  3. What one has not experienced, one will never understand in print

---
+++ converts_base.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * dec_to_bin - Converts unsigned decimal number to binary
+ *
+ * @args: Argument to print
+ *
+ * Return: Lenght of character printed
+ */
+int dec_to_bin(va_list args)
+{
+	int i, j, len = 0;
+	unsigned int num;
+	int bin_arr[256];
+
+	num = va_arg(args, unsigned int);
+
+	if (num == 0)
+		len += _putchar('0');
+
+	if (num < 1)
+		return (-1);
+
+	for (i = 0; num > 0; i++)
+	{
+		bin_arr[i] = num % 2;
+		num = num / 2;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+		len += _putchar(bin_arr[j] + '0');
+
+	return (len);
+}
--- print_digits.c
+++ print_digits.c
@@ -34,39 +34,3 @@ int p_nums(va_list args)
 	return (len);
 }
 
-
-/**
- * dec_to_bin - Converts unsigned decimal number to binary
- *
- * @args: Argument to print
- *
- * Return: Lenght of character printed
- */
-int dec_to_bin(va_list args)
-{
-	int i, j, len = 0;
-	unsigned int num;
-	int bin_arr[256];
-
-	num = va_arg(args, unsigned int);
-
-	if (num == 0)
-		len += _putchar('0');
-
-	if (num < 1)
-		return (-1);
-
-	if (num < 2)
-		len += _putchar(num);
-
-	for (i = 0; num > 0; i++)
-	{
-		bin_arr[i] = num % 2;
-		num = num / 2;
-	}
-
-	for (j = i - 1; j >= 0; j--)
-		len += _putchar(bin_arr[j] + '0');
-
-	return (len);
-}
