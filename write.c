9cce73abe3d7d01502e124c8b67ffea7b3605be5
Nacho369 <nachodev369@gmail.com>
2022-10-18 15:12:55 -0700

    Task 5. My weakness is wearing too much leopard print

--- write.c
+++ write.c
@@ -1,83 +1,32 @@
 #include "main.h"
 
-
-int check(const char *format, int count, int i);
-
 /**
- * _printf - Produce output according to specified
- * format
+ * _putchar - Writes the character c to stdout
  *
- * @format: Character string
+ * @c: The character to print
  *
- * Return: void
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set
+ * appropriately.
  */
-int _printf(const char *format, ...)
+int _putchar(char c)
 {
-	int i = 0, j, count, len = 0;
-	const int f_len = 9;
-	va_list arg_param;
-
-	args_t types[] = {
-		{'c', p_char}, {'s', p_str},
-		{'d', p_nums}, {'i', p_nums},
-		{'b', dec_to_bin}, {'u', p_uns_num},
-		{'o', dec_to_oct}, {'x', dec_to_hex},
-		{'X', dec_to_HEX}
-	};
-
-	if (format == NULL || (format[0] == '%' && format[1] == 0))
-		return (-1);
-
-	va_start(arg_param, format);
-
-	while (format != NULL && format[i])
-	{
-		if (format[i] != '%')
-			len += _putchar(format[i]);
-		else
-		{
-			i++;
-			if (format[i] == '%')
-				len += _putchar('%');
-
-			count = 0;
-
-			for (j = 0; j < f_len; j++)
-			{
-				if (format[i] == types[j].ch)
-				{
-					len += types[j].dt(arg_param);
-					count = 1;
-					break;
-				}
-			}
-			len += check(format, count, i);
-		}
-		i++;
-	}
-	va_end(arg_param);
-	return (len);
+	return (write(1, &c, 1));
 }
+
 /**
- * check - Check requirement
+ * _puts - Prints a string to stdout
  *
- * @format: Character string
- * @count: Check value
- * @i: integer iteration
+ * @str: Pointer to the string to print
  *
- * Return: len
+ * Return: Lumber of chars written
  */
-int check(const char *format, int count, int i)
+int _puts(char *str)
 {
-	int len = 0;
+	register int i;
 
-	if (!count && format[i] != '%')
-	{
-		len++;
-		len++;
-		_putchar('%');
-		_putchar(format[i]);
-	}
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
 
-	return (len);
+	return (i);
 }
