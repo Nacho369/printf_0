030c34b4282991949482c7b231160e4af5fdc8c3
Nacho369 <nachodev369@gmail.com>
2022-10-18 09:09:09 -0700

    Task 3. What one has not experienced, one will never understand in print

--- converts_base.c
+++ converts_base.c
@@ -73,34 +73,33 @@ int dec_to_oct(va_list args)
  */
 int dec_to_hex(va_list args)
 {
-	int i, j, len = 0;
+	int i, j, rem = 0, len = 0;
 	unsigned int num;
-	char hex_arr[256];
+	int hex_arr[256];
 
 	num = va_arg(args, unsigned int);
 
 	if (num == 0)
 		return (_putchar('0'));
-	if (num < 1)
-		return (-1);
 
-	for (i = 0; num > 0; i++)
+	for (i = 0; num != 0; i++)
 	{
-		int rem = num % 16;
+		rem = num % 16;
 
-		if (rem > 9)
+		if (rem < 10)
 		{
-			rem = hex_check(rem, 'x');
-			hex_arr[len] = rem;
+			hex_arr[i] = rem;
 		}
 		else
-			hex_arr[i] = rem + 48;
+			hex_arr[i] = rem + 39;
 
 		num = num / 16;
 	}
 
 	for (j = i - 1; j >= 0; j--)
+	{
 		len += _putchar(hex_arr[j] + '0');
+	}
 
 	return (len);
 }
@@ -116,7 +115,7 @@ int dec_to_hex(va_list args)
  */
 int dec_to_HEX(va_list args)
 {
-	int i, j, rem, len = 0;
+	int i, j, rem = 0, len = 0;
 	unsigned int num;
 	int HEX_arr[256];
 
@@ -125,17 +124,16 @@ int dec_to_HEX(va_list args)
 	if (num == 0)
 		len += _putchar('0');
 
-	for (i = 0; num > 0; i++)
+	for (i = 0; num != 0; i++)
 	{
 		rem = num % 16;
 
-		if (rem > 9)
+		if (rem < 10)
 		{
-			rem = hex_check(rem, 'X');
 			HEX_arr[i] = rem;
 		}
 		else
-			HEX_arr[i] = rem + 48;
+			HEX_arr[i] = rem + 7;
 
 		num = num / 16;
 	}
@@ -144,24 +142,4 @@ int dec_to_HEX(va_list args)
 		len += _putchar(HEX_arr[j] + '0');
 
 	return (len);
-}
-
-
-/**
- * hex_check - Checks which hex function is calling it
- * @num: Number to convert into letter
- * @x: Tells which hex function is calling it
- * Return: Ascii value for a letter
- */
-int hex_check(int num, char case_type)
-{
-	char *hex = "abcdef";
-	char *HEX = "ABCDEF";
-
-	num = num - 10;
-	if (case_type == 'x')
-		return (hex[num]);
-	else
-		return (HEX[num]);
-	return (0);
 }
